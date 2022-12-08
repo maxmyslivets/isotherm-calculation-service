8e3682f1d6090c24515e74a610a8eb2ce757f20b
maxmyslivets <by.mikky1206@gmail.com>
2022-12-08 22:29:31 +0300

    split page for upload and server for rest-api

--- cl.py
+++ cl.py
@@ -1,9 +1,8 @@
 import requests
 
-url = 'http://127.0.0.1:5000/im_size'
-my_img = {'image': open('E:/wall.jpg', 'rb')}
+url = 'http://127.0.0.1:3000/im_size'
+my_img = {'image': open('E:/Wall.jpg', 'rb')}
 r = requests.post(url, files=my_img)
-print(r)
 
 # convert server response into JSON format.
 print(r.json())
