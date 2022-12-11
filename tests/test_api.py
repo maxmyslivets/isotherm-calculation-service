a0088313575eb50144e47e7e24cd942e1a9e9a38
maxmyslivets <by.mikky1206@gmail.com>
2022-12-11 19:52:13 +0300

    edit tests for api

---
+++ tests/test_api.py
@@ -0,0 +1,30 @@
+from api import app
+from base64 import b64encode
+from image_processing.isotherm import Isotherm
+
+with open('E:/Wall.jpg', 'rb') as file:
+    binary_file = b64encode(file.read())
+
+
+def test_get_isotherm_status_code():
+
+    response_post = app.test_client().post("/get_isotherm", data={'image': binary_file})
+    assert response_post.status_code == 200
+
+    response_get = app.test_client().get("/get_isotherm")
+    assert response_get.status_code == 405
+
+
+def test_get_isotherm_content():
+
+    response = app.test_client().post("/get_isotherm", data={'image': binary_file})
+
+    assert "status" in response.json.keys()
+    assert "isotherm" in response.json.keys()
+
+
+def test_get_isotherm_parse():
+    response = app.test_client().post("/get_isotherm", data={'image': binary_file})
+    isotherm = Isotherm(response.json["isotherm"])
+
+    assert isotherm
