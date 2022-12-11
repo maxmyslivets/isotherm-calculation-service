e0ab34fd52a8bbb5b9e732cca2ce44b4f3daff03
maxmyslivets <by.mikky1206@gmail.com>
2022-12-11 19:46:13 +0300

    added dataclasses for isotherms

---
+++ image_processing/isotherm.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass()
+class Point:
+    x: int
+    y: int
+
+    def __repr__(self) -> str:
+        return f"Point({self.x}, {self.y})"
+
+
+@dataclass()
+class Isoline:
+    points: list[Point]
+    level: float
+
+    def __repr__(self) -> str:
+        return f"Isoline (l={self.level}, n={len(self.points)})"
+
+
+@dataclass
+class Isotherm:
+    data: dict
+    isolines: list[Isoline]
+
+    def __post_init__(self) -> None:
+        # deserialize data to list[Isoline]
+        pass
+
+    def __repr__(self) -> str:
+        return f"Isotherm (n={len(self.isolines)})"
