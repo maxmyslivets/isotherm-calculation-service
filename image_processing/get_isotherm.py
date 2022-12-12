ae64caa119c8f7601f28edac4c3e9ea306ff9ba8
maxmyslivets <by.mikky1206@gmail.com>
2022-12-12 18:50:14 +0300

    added plug for upload image

--- image_processing/get_isotherm.py
+++ image_processing/get_isotherm.py
@@ -8,7 +8,12 @@ from image_processing.marker import Marker
 
 def get_isotherm(image: bytes, markers: dict) -> Isotherm:
 
-    img = imread(BytesIO(image))    # FIXME: read from a url
+    # img = imread(url)
+    # FIXME: read from a url
+    #  изображение заливается на фронт, оттуда берем url и пихаем в post запрос
+    # заглушка
+    img = imread('tests/media/DJI_0258.JPG')
+
     markers_ = [Marker(*values) for marker, values in markers.items()]
 
     # TODO: img >> x,y,z arrays from cv
